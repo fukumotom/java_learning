ddd79252c186603ac17a538b567b08f7ff9204d3
fukumotom <a.word.of.kindness073@gmail.com>
2016-03-01 23:54:26 +0900

    form Authentication

--- 04_製造・UT/sql/createTable.sql
+++ 04_製造・UT/sql/createTable.sql
@@ -4,14 +4,14 @@ drop table Users;
 
 create table Users
 (
-user_id serial NOT NULL  PRIMARY KEY
-,user_name VARCHAR(20) NOT NULL
+user_id serial NOT NULL
+,user_name VARCHAR(20) NOT NULL PRIMARY KEY
 ,password VARCHAR(20) NOT NULL
 );
 
 create table UserRole
 (
-user_id serial NOT NULL REFERENCES Users(user_id)
+user_name VARCHAR(20) NOT NULL REFERENCES Users(user_name)
 ,role VARCHAR(5) NOT NULL
 );
 
