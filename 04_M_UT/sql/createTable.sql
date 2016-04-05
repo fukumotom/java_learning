2219b795b44c7781274ca3e40dce17f978c51a77
fukumotom <a.word.of.kindness073@gmail.com>
2016-04-06 00:13:33 +0900

    modify sql file

--- 04_M_UT/sql/createTable.sql
+++ 04_M_UT/sql/createTable.sql
@@ -4,7 +4,6 @@ drop table Users;
 
 create table Users
 (
-user_id serial NOT NULL
 ,user_name VARCHAR(20) NOT NULL PRIMARY KEY
 ,password VARCHAR(64) NOT NULL
 );
@@ -18,7 +17,7 @@ user_name VARCHAR(20) NOT NULL REFERENCES Users(user_name)
 create table WorkManage
 (
 id serial NOT NULL
-,user_id integer NOT NULL
+,user_name VARCHAR(20) NOT NULL
 ,startTime time(0) NOT NULL
 ,endTime time(0) NOT NULL
 ,workingTime time(0) NOT NULL
---
+++ 04_M_UT/sql/sampleDate_workmanage.sql
@@ -0,0 +1,27 @@
+INSERT INTO WorkManage(
+user_name
+,startTime
+,endTime
+,workingTime
+,contents
+,note
+,worktime
+,uptime
+,deleteFlg
+,insertFlg
+,updateFlg
+
+) VALUES (
+
+'user1'
+,'00:00:00'
+,'00:00:00'
+,'00:00:00'
+,'サンプル内容'
+,'サンプルメモ'
+,'2016-03-05'
+,TIMESTAMP '2016-03-05 00:00:00'
+,0
+,0
+,0
+);
